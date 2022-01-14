797d08ecf678e57ab63d8d5b4f1d4c447cb0bb53
Silas Santos da Silva <silvasilas66@gmail.com>
2022-01-14 15:04:39 -0300

    Creating game model and migration

---
+++ app/Models/Game.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Game extends Model
+{
+    use HasFactory;
+    
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'developer',
+        'set_in',
+        'protagonist',
+        'location',
+    ];
+}
