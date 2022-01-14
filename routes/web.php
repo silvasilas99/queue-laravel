5705c09fb9ff183cdf46d416187970367fd40a06
Silas Santos da Silva <silvasilas66@gmail.com>
2022-01-14 18:48:33 -0300

    Creating web routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\GameController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [GameController::class, 'index'])->name('index');
+Route::get('/create', [GameController::class, 'create'])->name('create');
+Route::post('/store', [GameController::class, 'store'])->name('store');
+Route::get('/get_csv', [GameController::class, 'getCsvFile'])->name('get_csv');
